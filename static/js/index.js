5fbccce8d8015f15defeebcfc08b84ce140c3e56
SharapaGorg <sharapov.savely@yandex.ru>
2022-05-31 13:01:40 +0300

    added bin form

--- static/js/index.js
+++ static/js/index.js
@@ -6,6 +6,73 @@ window.addEventListener('load', () => {
     //
 });
 
+// UTILS
+function createElement(tagName, className) {
+    let element = document.createElement(tagName)
+    element.className = className
+
+    return element
+}
+
+function capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1)
+}
+
+// BIN FORM
+let binButton = window.document.getElementById('bin')
+let binWrapper = document.getElementById('binWrapper')
+let bin = document.getElementById('binForm')
+let crossBin = document.getElementById('crossBin')
+let binItems = document.getElementById("binList")
+
+// show bin form
+binButton.addEventListener("click", () => {
+    binWrapper.style.display = 'block'
+    bin.style.display = 'block'
+})
+
+// hide bin form
+crossBin.addEventListener("click", () => {
+    binWrapper.style.display = 'none'
+    bin.style.display = 'none'
+})
+
+function getItem(imgSrc, title, price) {
+    let itemWrapper = createElement("div", "bin-item grid grid-cols-2 justify-items-center")
+    let imgWrapper = createElement("div", "bin-img-wrapper")
+    let image = createElement("img", "bin-img mx-auto")
+    let itemDescription = createElement("div", "bin-item-desc")
+    let itemTitle = createElement("div", "bin-item-title text-center")
+    let itemPrice = createElement("div", "bin-item-price text-center")
+    let removeButton = createElement("div", "remove_from_bin px-2 mx-auto")
+
+    image.src = imgSrc
+    itemTitle.innerText = title
+    itemPrice.innerText = price + ' rub'
+    removeButton.innerText = 'remove -'
+
+    imgWrapper.appendChild(image)
+    
+    itemDescription.appendChild(itemTitle)
+    itemDescription.appendChild(itemPrice)
+    itemDescription.appendChild(removeButton)
+
+    itemWrapper.appendChild(imgWrapper)
+    itemWrapper.appendChild(itemDescription)
+
+    // remove item from bin
+    removeButton.addEventListener("click", () => {
+        itemWrapper.parentNode.removeChild(itemWrapper)
+    })
+
+    return itemWrapper
+}
+
+function addItemToBin(imageSrc, title, price) {
+    let item = getItem(imageSrc, title, price)
+    binItems.appendChild(item)
+}
+
 // SIDEBAR ACTIONS
 let selectedCategory = 'all'
 let categories = document.getElementsByClassName('category')
@@ -44,17 +111,6 @@ for (let category of categories) {
     })
 }
 
-function createElement(tagName, className) {
-    let element = document.createElement(tagName)
-    element.className = className
-
-    return element
-}
-
-function capitalize(string) {
-    return string[0].toUpperCase() + string.slice(1)
-}
-
 
 // FIGURE PRODUCT CELL
 function getProduct(imgSrc, category, title, price) {
@@ -63,11 +119,13 @@ function getProduct(imgSrc, category, title, price) {
     let category_ = createElement("span", "cat_product ")
     let title_ = createElement("span", "text-3xl font-bold title_product")
     let price_ = createElement("span", "price_product text-2xl")
+    let addToBin = createElement("div", "add_to_bin noselect")
 
     image.src = imgSrc
     category_.innerText = category
     title_.innerText = title
     price_.innerText = price + ' rub'
+    addToBin.innerText = 'add +'
 
     let headersWrapper = createElement("div", "px-2 py-1")
     let imageWrapper = createElement("div", "img_product_wrapper")
@@ -82,6 +140,19 @@ function getProduct(imgSrc, category, title, price) {
     headersWrapper.appendChild(category_)
     headersWrapper.innerHTML += '<br>'
     headersWrapper.appendChild(price_)
+
+    wrapper.appendChild(addToBin)
+
+    addToBin.addEventListener("click", () => {
+        if (addToBin.innerText !== 'added') {
+            addItemToBin(imgSrc, title, price)
+        }
+
+        addToBin.innerText = 'added'
+
+        addToBin.style.color = 'rgb(247, 241, 241)'
+        addToBin.style.background = '#28cb59'
+    })
 
     return wrapper
 }
@@ -118,7 +189,7 @@ async function renderShoes(title) {
 let accountButton = window.document.getElementById("account")
 let authWrapper = document.getElementById("authWrapper")
 let authForm = document.getElementById("authForm")
-let cross = document.getElementById("cross")
+let cross = document.getElementById("crossAuth")
 
 // activate auth form
 accountButton.addEventListener("click", () => {
@@ -126,7 +197,7 @@ accountButton.addEventListener("click", () => {
     authForm.style.display = 'block'
 })
 
-// hidden auth form
+// hide auth form
 cross.addEventListener("click", () => {
     authWrapper.style.display = 'none'
     authForm.style.display = 'none'
@@ -169,6 +240,7 @@ signIn.addEventListener("click", () => {
 signUp.addEventListener("click", () => {
     //
 })
+
 
 renderShoes('all')
 // monitor search field
